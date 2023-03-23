796598414586e9c90fbed7585abd3879af99b85a
bozhidar.stoyanov <bozhidar.stoyanov@epicdevs.io>
2023-03-23 22:36:39 +0200

    Enable signing any text message (not just for burning tokens).

--- src/web_usage.js
+++ src/web_usage.js
@@ -2,31 +2,25 @@ import { ethers } from "https://cdn-cors.ethers.io/lib/ethers-5.5.4.esm.min.js";
 
 export default function Web3Sdk() {
 
-    const getOffChainSignature = async (tokenIds = []) => {
+    const getOffChainSignature = async (message) => {
 
         try {
             if (!window.ethereum)
                 throw new Error("No crypto wallet found. Please install it.");
 
-            if(tokenIds.length === 0)
-                throw new Error("No token ids found.");
+            if(message === null)
+                throw new Error("No message to sign.");
 
             await window.ethereum.send("eth_requestAccounts");
             const provider = new ethers.providers.Web3Provider(window.ethereum);
 
             const signer = provider.getSigner();
 
-            let msg = `
-            Dear user!
-            
-            You are about to burn the tokenIDs below:
-            ${tokenIds.join(',')}
-            Please sign the request.`
-
-            const signature = await signer.signMessage(msg);
+            const signature = await signer.signMessage(message);
+            const byteArray = new Uint8Array(new TextEncoder().encode(signature.slice(2)));
             const address = await signer.getAddress();
 
-            return {address, signature};
+            return {signature, byteArray};
         } catch (err) {
             throw new Error(err.message);
         }
